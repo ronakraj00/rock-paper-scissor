f284477a8c1fb968a612067cf0568acc3b80cd30
ronak raj <126279165+ronakraj00@users.noreply.github.com>
2023-07-26 20:47:08 +0530

    add images,icon,playerPoints,compPoints

--- script.js
+++ script.js
@@ -1,163 +1,132 @@
 // variables for counting
-
-let countP=0;
-let countC=0;
-let round =0;
-
-
+let countP = 0;
+let countC = 0;
+let round = 0;
 
 // DOM manipulation
-const container=document.querySelector('.container');
-const statDiv=document.createElement('div');
-const statPara=document.createElement('p');
-
+const container = document.querySelector(".container");
+const statDiv = document.createElement("div");
+const statPara = document.createElement("p");
+const playerPoints = document.querySelector("#player-points span");
+const computerPoints = document.querySelector("#computer-points span");
+const roundNo = document.querySelector("#round-no span");
 
 //function make status all zero
-function everyZero(){
-    countC=0;
-    countP=0;
-    round=0;
-    statPara.textContent="New round has started\n select your choice"
+function everyZero() {
+  countC = 0;
+  countP = 0;
+  round = 0;
+  statPara.textContent = "New round has started\n select your choice";
+  playerPoints.textContent = "0";
+  computerPoints.textContent = "0";
+  roundNo.textContent = "0";
 }
-
-
-//console game start
-console.log("Game Start");
 
 //gets the choice from computer
 function getComputerChoice() {
-    return Math.floor((Math.random() * 3) + 1);
+  return Math.floor(Math.random() * 3 + 1);
 }
-
 
 //function for each round
 function playRound(player, comp) {
-    console.log(`${player}:${comp}`)
-    if (player === comp) {
-        statPara.textContent="TIE";
-        return;
-    }
-    else if (player === 1 && comp === 3) {
-        statPara.textContent="Player has won this round";
+  if (player === comp) {
+    statPara.textContent = "TIE";
+    return;
+  } else if (player === 1 && comp === 3) {
+    statPara.textContent = "Player has won this round";
 
-        return countP+=1;
-    }
-    else if (player === 1 && comp === 2) {
-        statPara.textContent="Computer has won this round";
+    return (countP += 1);
+  } else if (player === 1 && comp === 2) {
+    statPara.textContent = "Computer has won this round";
 
-        return countC+=1;
-    }
-    else if (player === 2 && comp === 3) {
-        statPara.textContent="Computer has won this round";
-        return countC+=1;
-    }
-    else if (player === 2 && comp === 1) {
-        statPara.textContent="Player has won this round";
-        return countP+=1;
-    }
-    else if (player === 3 && comp === 1) {
-        statPara.textContent="Computer has won this round";
-        return countC+=1;
-    }
-    else if (player === 3 && comp === 2) {
-        statPara.textContent="Player has won this round";
-        return countP+=1;
-    }
+    return (countC += 1);
+  } else if (player === 2 && comp === 3) {
+    statPara.textContent = "Computer has won this round";
+    return (countC += 1);
+  } else if (player === 2 && comp === 1) {
+    statPara.textContent = "Player has won this round";
+    return (countP += 1);
+  } else if (player === 3 && comp === 1) {
+    statPara.textContent = "Computer has won this round";
+    return (countC += 1);
+  } else if (player === 3 && comp === 2) {
+    statPara.textContent = "Player has won this round";
+    return (countP += 1);
+  }
 }
 
-//game function 
-function game(playerChoice){
-    playRound(playerChoice,getComputerChoice());
-    round+=1;
-    if(round==5){
-        statPara.innerHTML=`All 5 rounds completed <br> ${whoWon(countC,countP)}`
-        reset();
+//game function
+function game(playerChoice) {
+  if (round < 5) {
+    playRound(playerChoice, getComputerChoice());
+    playerPoints.textContent = countP;
+    computerPoints.textContent = countC;
+    roundNo.textContent = round + 1;
+    round += 1;
+
+    //color the point of winner (only round wise)
+    if (countP > countC) {
+      playerPoints.classList.add("winning");
+      computerPoints.classList.remove("winning");
     }
-    
+    if (countC > countP) {
+      computerPoints.classList.add("winning");
+      playerPoints.classList.remove("winning");
+    }
+  }
+  if (round == 5) {
+    statPara.innerHTML = `All 5 rounds completed <br> ${whoWon(
+      countC,
+      countP
+    )}`;
+    reset();
+    round += 1;
+  }
 }
 
-
-//reset function 
-function reset(){
-    const resetB=document.createElement('button');
-    resetB.textContent="Reset";
-    
-    container.appendChild(resetB);
-    resetB.addEventListener('click',(e)=>{
-        e.stopPropagation();
-        everyZero();
-        container.removeChild(resetB);
-
-    })
+//reset function
+function reset() {
+  const resetB = document.createElement("button");
+  resetB.textContent = "Reset";
+  container.appendChild(resetB);
+  resetB.addEventListener(
+    "click",
+    (e) => {
+      e.stopPropagation();
+      everyZero();
+      container.removeChild(resetB);
+    },
+    { once: true }
+  );
 }
-
-
 
 //function to decide who won after all rounds
-function whoWon(countC,countP){
-    if(countC===countP){
-        return "tie";
-    }
-    else if( countC>countP){
-        return "Computer won by point --> "+countC;
-    }
-    else if(countP>countC){
-        return "You have won!! by points --> "+countP;
-    }
+function whoWon(countC, countP) {
+  if (countC === countP) {
+    return "tie";
+  } else if (countC > countP) {
+    return "Computer won by point --> " + countC;
+  } else if (countP > countC) {
+    return "You have won!! by points --> " + countP;
+  }
 }
-
 
 //DOM manipulation
 container.appendChild(statDiv);
 container.appendChild(statPara);
 
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissor = document.querySelector("#scissor");
 
-
-
-
-
-
-const rock = document.querySelector('#rock');
-const paper = document.querySelector('#paper');
-const scissor = document.querySelector('#scissor');
-
-
-if(round<5){
-    rock.addEventListener('click',runfn1);
-    paper.addEventListener('click',runfn2);
-    scissor.addEventListener('click',runfn3 );
-}   
-
-
-function runfn1(e){
+//button click event
+rock.addEventListener("click",()=>{
     game(1);
-
-    e.stopPropagation();
-}
-function runfn2(e){
+});
+paper.addEventListener("click",()=>{
     game(2);
-
-    e.stopPropagation();
-}
-function runfn3(e){
+});
+scissor.addEventListener("click",()=>{
     game(3);
-
-    e.stopPropagation();
-}
-
-        
-
- 
-
-
-
-
-
-
-
-
-
-
-
-
+});
 
